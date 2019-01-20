105ab01918dcbbcbba874ef6c46d989c7e7b8a18
Juan Manuel Torres <kinojman@gmail.com>
2019-01-20 14:58:42 -0800

    Moving subscription method to the logic

--- src/main/scala/io/onema/userverless/overwatch/registration/LogRegistrationFunction.scala
+++ src/main/scala/io/onema/userverless/overwatch/registration/LogRegistrationFunction.scala
@@ -27,9 +27,9 @@ class LogRegistrationFunction extends LambdaHandler[CloudWatchLogEvent, Unit] wi
   //--- Fields ---
   val client: AWSLogs = AWSLogsClientBuilder.defaultClient()
   val retentionTime: Int = getValue("log/retention/days").getOrElse("7").toInt
-  val logic: LogRegistrationLogic = new LogRegistrationLogic(client)
   val destinationFunc: Option[String] = getValue("destination/function")
   val logGroupPrefix: String = getValue("log/group/prefix").getOrElse("/aws/lambda")
+  val logic: LogRegistrationLogic = new LogRegistrationLogic(client, logGroupPrefix)
 
   //--- Methods ---
   override def execute(event: CloudWatchLogEvent, context: Context): Unit = {
@@ -42,20 +42,9 @@ class LogRegistrationFunction extends LambdaHandler[CloudWatchLogEvent, Unit] wi
 
         // Get own self log group to avoid self registration
         val accountId = context.accountId.getOrElse("")
-        destinationFunc.foreach(x => subscribe(x, accountId, logGroup))
+        destinationFunc.foreach(func => logic.subscribe(func, accountId, logGroup, stageName, region))
       case None =>
         log.warn(s"Registration was not successful as the `detail.requestParameters` is null. Message will be ignored. Error: ${event.detail.errorMessage.getOrElse("No error available")}")
-    }
-  }
-
-  def subscribe(destinationFunc: String, accountId: String, logGroup: String): Unit = {
-    val destinationFuncArn = s"arn:aws:lambda:$region:$accountId:functionArn:$destinationFunc"
-
-    // Ignore the destination functionArn itself to avoid invocation loops and logs that do not use the configured prefix
-    if(logGroup == s"$logGroupPrefix/$destinationFunc" || !logGroup.startsWith(logGroupPrefix) || logGroup.startsWith(s"/aws/lambda/overwatch") || !logGroup.contains(stageName)) {
-      log.info(s"""Ignoring the destination functionArn $destinationFuncArn.""")
-    } else {
-      logic.updateSubscriptionFilter(logGroup, destinationFuncArn)
     }
   }
 
--- src/main/scala/io/onema/userverless/overwatch/registration/LogRegistrationLogic.scala
+++ src/main/scala/io/onema/userverless/overwatch/registration/LogRegistrationLogic.scala
@@ -15,13 +15,24 @@ import com.amazonaws.services.logs.AWSLogs
 import com.amazonaws.services.logs.model.{PutRetentionPolicyRequest, PutSubscriptionFilterRequest}
 import com.typesafe.scalalogging.Logger
 
-class LogRegistrationLogic(val logClient: AWSLogs) {
+class LogRegistrationLogic(val logClient: AWSLogs, val logGroupPrefix: String) {
 
   //--- Fields ---
   val log = Logger(classOf[LogRegistrationLogic])
-  val filterPattern: String = """?"*** DEBUG" ?"*** INFO" ?"*** WARN" ?"ERROR" ?"*** METRIC" "REPORT" """
+  val filterPattern: String = """?"*** DEBUG" ?"*** INFO" ?"*** WARN" ?"ERROR" ?"*** METRIC" ?"REPORT" """
 
   //--- Methods ---
+  def subscribe(destinationFunc: String, accountId: String, logGroup: String, stageName: String, region: String): Unit = {
+    val destinationFuncArn = s"arn:aws:lambda:$region:$accountId:function:$destinationFunc"
+
+    // Ignore the destination functionArn itself to avoid invocation loops and logs that do not use the configured prefix
+    if(logGroup == s"$logGroupPrefix/$destinationFunc" || !logGroup.startsWith(logGroupPrefix) || logGroup.startsWith(s"/aws/lambda/overwatch") || !logGroup.contains(stageName)) {
+      log.info(s"""Ignoring the destination functionArn $destinationFuncArn.""")
+    } else {
+      updateSubscriptionFilter(logGroup, destinationFuncArn)
+    }
+  }
+
   def updateRetentionPolicy(logGroup: String, retentionTime: Int): Unit = {
     log.info(s"""Setting the retention policy for "$logGroup" to $retentionTime days.""")
     val request = new PutRetentionPolicyRequest(logGroup, retentionTime)
@@ -29,7 +40,7 @@ class LogRegistrationLogic(val logClient: AWSLogs) {
   }
 
   def updateSubscriptionFilter(logGroup: String, functionArn: String): Unit = {
-    log.info(s"""Setting subscription filter for "$logGroup" """)
+    log.info(s"""Setting subscription filter for "$logGroup" to "$functionArn" """)
     val request = new PutSubscriptionFilterRequest()
       .withFilterName("µserverless-logs")
       .withDestinationArn(functionArn)
