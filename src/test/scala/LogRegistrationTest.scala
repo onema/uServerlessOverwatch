b75d4899ffdecccd6777ee4663755fea8b80edc0
Juan Manuel Torres <kinojman@gmail.com>
2019-01-19 23:18:12 -0800

    Update tests

--- src/test/scala/LogRegistrationTest.scala
+++ src/test/scala/LogRegistrationTest.scala
@@ -1,9 +1,6 @@
-import java.io.ByteArrayInputStream
-
-import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext
 import com.amazonaws.services.logs.AWSLogs
-import io.onema.userverless.events.LogRegistration.LogCreationEvent
-import io.onema.userverless.overwatch.logs.{LogRegistrationFunction, LogRegistrationLogic}
+import io.onema.userverless.events.CloudTrailCloudWatchEvent.CloudWatchLogEvent
+import io.onema.userverless.overwatch.registration.{LogRegistrationFunction, LogRegistrationLogic}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -19,10 +16,10 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 
 class LogRegistrationTest extends FlatSpec with Matchers with MockFactory {
-  "A function LogRegistrationFunction" should "decode payload" in {
+  "A functionArn LogRegistrationFunction" should "decode payload" in {
     // Arrange
     class LRFTest extends LogRegistrationFunction {
-      def doJsonDecode(json: String): LogCreationEvent = jsonDecode(json)
+      def doJsonDecode(json: String): CloudWatchLogEvent = jsonDecode(json)
     }
     val event = """{"version":"0","id":"64fbda4e-5992-2224-6f33-3c8efe447fbb","detail-type":"AWS API Call via CloudTrail","source":"aws.logs","account":"123456789012","time":"2018-11-14T04:49:37Z","region":"us-east-1","resources":[],"detail":{"eventVersion":"1.04","userIdentity":{"type":"IAMUser","principalId":"AIDAJ4W5SF5AANMKCKRVW","arn":"arn:aws:iam::123456789012:user/juant-development","accountId":"123456789012","accessKeyId":"ASIAJB2QWB2IWP2O4GGQ","userName":"juant-development","sessionContext":{"attributes":{"mfaAuthenticated":"false","creationDate":"2018-11-14T04:49:33Z"}},"invokedBy":"cloudformation.amazonaws.com"},"eventTime":"2018-11-14T04:49:37Z","eventSource":"logs.amazonaws.com","eventName":"CreateLogGroup","awsRegion":"us-east-1","sourceIPAddress":"cloudformation.amazonaws.com","userAgent":"cloudformation.amazonaws.com","requestParameters":{"logGroupName":"/aws/lambda/echoapi"},"responseElements":null,"requestID":"b0b70559-e7c8-11e8-bb2f-bd281a5f414c","eventID":"efb309eb-d05e-40ee-bac8-0ff6adfb8ee3","eventType":"AwsApiCall","apiVersion":"20140328"}}"""
     val func = new LRFTest()
@@ -37,7 +34,7 @@ class LogRegistrationTest extends FlatSpec with Matchers with MockFactory {
 
   }
 
-  "A function LogRegistrationFunction" should "not allow subscription if it is an overwatch function" in {
+  "A functionArn LogRegistrationFunction" should "not allow subscription if it is an overwatch functionArn" in {
     // Arrange
     class LRFTest(logsClient: AWSLogs) extends LogRegistrationFunction {
       override val logic: LogRegistrationLogic = new LogRegistrationLogic(logsClient)
